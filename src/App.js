249d17957afe4a3408f4cd6013ae5781cbcf59f1
Nikola Trajkovic <nikolatrajkovic.contact@gmail.com>
2019-11-01 18:24:00 +0100

    Setup the project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return <div className='App'>Hello World</div>;
-  }
+import HomePage from './pages/homepage/homepage.component';
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
