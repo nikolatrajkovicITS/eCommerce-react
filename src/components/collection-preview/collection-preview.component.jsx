995caf5b31cb09380a7a31bb14fa71d37e3a0834
Nikola Trajkovic <nikolatrajkovic.contact@gmail.com>
2019-11-03 12:19:03 +0100

    Created shop page

---
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "./collection-preview.styles.scss";
+
+const CollectionPreview = ({ title, items }) => (
+  <div className='collection-preview'>
+    <h1 className='title'>{title.toUpperCase()}</h1>
+    <div className='preview'>
+      {items
+        .filter((item, idx) => idx < 4)
+        .map(({ id, name }) => (
+          <div key={id}>{name}</div>
+        ))}
+    </div>
+  </div>
+);
+
+export default CollectionPreview;
