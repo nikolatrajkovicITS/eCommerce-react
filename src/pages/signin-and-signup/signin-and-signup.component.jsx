43309400fcb5b2bf7c6adcf062d9eda966a46739
Nikola Trajkovic <nikolatrajkovic.contact@gmail.com>
2019-11-11 18:44:56 +0100

    Created sign-in page and form

---
+++ src/pages/signin-and-signup/signin-and-signup.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./signin-and-signup.styles.scss";
+import SignIn from "../../components/sgin-in/sing-in.component";
+
+const SignInAndSignUp = () => (
+  <div className='sign-in-and-sign-up'>
+    <SignIn />
+  </div>
+);
+
+export default SignInAndSignUp;
