bee858883496ec27e8f76459e534ddb70f3b4f04
Nikola Trajkovic <nikolatrajkovic.contact@gmail.com>
2020-01-24 16:08:20 +0100

    Data normalization for collections and improved collecton selector

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,11 +3,8 @@ import { withRouter } from "react-router-dom";
 
 import "./menu-item.styles.scss";
 
-const MenuItem = ({ title, imageUrl, size, history, linkUrl, baseUrl }) => (
-  <div
-    className={`${size} menu-item`}
-    onClick={() => history.push(`${baseUrl}/${linkUrl}`)}
-  >
+const MenuItem = ({ title, imageUrl, size, history, linkUrl }) => (
+  <div className={`${size} menu-item`} onClick={() => history.push(linkUrl)}>
     <div
       className="background-image"
       style={{
