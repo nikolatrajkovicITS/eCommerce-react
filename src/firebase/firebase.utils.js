4e454a87570c4fec18456a7336058ea5c9012c46
Nikola Trajkovic <nikolatrajkovic.contact@gmail.com>
2020-02-04 11:23:51 +0100

    Moving shop data to firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -35,6 +35,11 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   return userRef;
 };
 
+export const addCollectionAndDocuments = (collectionKey, objectToAdd) => {
+  const collectionRef = firestore.collection(collectionKey);
+  console.log(collectionRef);
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
